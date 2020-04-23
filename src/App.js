9f1b9271df14402981a2ad764830e2ffd354819a
Uray Jordi Lasardi <u.jordi@smooets.com>
2020-04-23 14:18:18 +0700

    Slicing Component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const category = "Running";
-  const price = 74000000;
-
   return (
     <div className="ParentBox">
-      <div className="Image">
-        <img src="sample.jpg" />
-      </div>
+      <Image />
+      <ProdukInfo category="Sneakers" name="Black Swagg" />
+    </div>
+  );
+}
+
+function Image() {
+  return (
+    <div className="Image">
+      <img src="sample.jpg" />
+    </div>
+  );
+}
+
+function ProdukInfo(props) {
+  const {category, name} = props ;
+
+  return (
+    <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
-        <h1 className="Title">Black Swagg</h1>
-        <p className="Price">IDR {price}</p>
+        <h1 className="Title">{name}</h1>
+        <p className="Price">IDR 7.400.999</p>
         <p className="Info">One of the most react shoes in the AJ 
         Collection, Black sneakers have earned their own aesthetic ranking in the modern male wardrobe. As a staple footwear piece they can be paired with various popular looks ranging from classic denim to luxe streetwear to leather jackets.</p>
       </div>
