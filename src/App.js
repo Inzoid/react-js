49f7503cfc9e79b2ed0c8224acedbd93cbb9dd06
Uray Jordi Lasardi <u.jordi@smooets.com>
2020-04-23 13:36:03 +0700

    design layout

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const category = "Running";
+  const price = 74000000;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ParentBox">
+      <div className="Image">
+        <img src="sample.jpg" />
+      </div>
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title">Black Swagg</h1>
+        <p className="Price">IDR {price}</p>
+        <p className="Info">One of the most react shoes in the AJ 
+        Collection, Black sneakers have earned their own aesthetic ranking in the modern male wardrobe. As a staple footwear piece they can be paired with various popular looks ranging from classic denim to luxe streetwear to leather jackets.</p>
+      </div>
     </div>
   );
 }
