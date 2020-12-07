447a1c85609786534b4250c3a32c096814ba40c8
Inzoid <inzoid21@gmail.com>
2020-12-07 09:50:30 +0700

    change to hooks function and styling

--- src/App.js
+++ src/App.js
@@ -1,99 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ReviewItems from './Reviews';
-import PropTypes from 'prop-types';
 
   function App() {
+    const [changeName, setChangeName] = useState('Dior Limited')
+    const [isDiscount, setIsDiscount] = useState('coming')
+    const [cart, setCart] = useState(0);
+    const [value, setValue] = useState(0);
+  
+    const handlePlus = () => {
+      setValue(value + 1)
+    }
+
+    const handleMinus = () => {
+      setValue(value - 1)
+    }
+
+    const chageNameHandler = () => {
+      setChangeName('Air Jordan')
+    }
+
+    const changeDiscount = () => {
+      setIsDiscount('yes')
+      return alert('Press OK to see Discount ')
+    }
+
+    const handleAddCart = (e) => {
+      setCart(cart + 1)
+      return alert(('Add Item ' + e + ' Success'));
+    }
+
+    const Header = () => {
+      return(
+        <div className="header">
+          <div className="troley"><br />
+              <div className="count">{cart}</div>
+          </div>
+        </div>
+      )
+    }
+
+    const Footer = () => {
+      return(
+        <div className="header">
+        </div>
+      )
+    }
+
+    const Image = () => {
+      return (
+        <div className="Image">
+          <img src="sample.jpg" />
+        </div>
+      );
+    }
+
+    const ProdukInfo = props => {
+      const {category, name, isDiscount} = props ;
+      const benefit = ["New Space", "Best Quality", "Exclusive"];
+      const listBenefit = benefit.map((itemBenefit) =>
+        <li key={itemBenefit}> { itemBenefit } </li>
+      );
+
+      return (
+        <div>
+          <div className="Deskripsi">
+            <b className="Cate">{category}</b>
+              <h1 className="Title">{name} <button onClick={chageNameHandler} className="btn-name">Change Name</button></h1> 
+                <p className="Price">IDR 7.399.999</p>
+                {isDiscount == 'yes' &&
+                  <b style={{color: 'teal'}}>Congrats Discount added</b>
+                }
+                <CheckDiscount isDiscount={isDiscount} discount={40} />
+                <p className="Info">One of the most best shoes in the AJ 
+                Collection, Black sneakers have earned their own aesthetic ranking in the modern male wardrobe. As a staple footwear piece they can be paired with various popular looks ranging from classic denim to luxe streetwear to leather jackets.</p>
+              <ul>
+                { listBenefit }
+              </ul>
+            <div className="counter">
+              <button onClick={(e) => handleAddCart(name, e)}  className="btn">Add to Cart</button>
+              <button className="minus" onClick={handlePlus}>+</button>
+                <input type="text" value={value} />
+              <button className="plus" onClick={handleMinus}>-</button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    const CheckDiscount = props => {
+      const { isDiscount, discount } = props;
+  
+      if(isDiscount == "yes"){
+        return(
+          <p>Discount <b>{discount}%</b> off</p>
+        );
+      }
+      else if(isDiscount == "coming"){
+        return(
+          <p>Discount <button onClick={changeDiscount} className="btn-discount">See Discount</button>
+          </p>
+        );
+      }
+      else {
+        return(
+          <p>Not Discount</p>
+        );
+      }
+
+    }
+    
     return (
       <div>
         <Header />
         <div className="ParentBox">
           <Image />
-          <ProdukInfo category="Sneakers" name="Black Swagg" isDiscount="yes" />
-          <ReviewItems />
-  
-          <Image />
-          <ProdukInfo category="Sport" name="White Sneakers" isDiscount="coming" />
+          <ProdukInfo category="Sneakers" name={changeName} isDiscount={isDiscount} />
           <ReviewItems />
         </div>
+        <Footer />
       </div>
     );
-  }
-
-  function Image() {
-    return (
-      <div className="Image">
-        <img src="sample.jpg" />
-      </div>
-    );
-  }
-
-  function CheckDiscount(props) {
-    const { isDiscount, discount } = props;
-
-    if(isDiscount == "yes"){
-      return(
-        <p>Discount {discount}% off</p>
-      );
-    }
-    else if(isDiscount == "coming"){
-      return(
-        <p>Discount Coming Soon</p>
-      );
-    }
-    else {
-      return(
-        <p>Not Discount</p>
-      );
-    }
-  }
-
-  function ProdukInfo(props) {
-    const {category, name, isDiscount} = props ;
-    const benefit = ["New Space", "Best Quality", "Exclusive"];
-    const listBenefit = benefit.map((itemBenefit) =>
-      <li key={itemBenefit}> { itemBenefit } </li>
-    );
-
-    return (
-      <div>
-        <div className="Deskripsi">
-          <p className="Cate">{category}</p>
-            <h1 className="Title">{name}</h1>
-              <p className="Price">IDR 7.399.999</p>
-                <CheckDiscount isDiscount={isDiscount} discount={40} />
-                  <p className="Info">One of the most best shoes in the AJ 
-                  Collection, Black sneakers have earned their own aesthetic ranking in the modern male wardrobe. As a staple footwear piece they can be paired with various popular looks ranging from classic denim to luxe streetwear to leather jackets.</p>
-                  <ul>
-                    { listBenefit }
-                  </ul>
-          <div className="counter">
-              <button onClick={(e) => AddCart(name, e)} className="btn">Add to Cart</button>
-              <button className="minus">-</button>
-                <input type="text" value="1"/>
-              <button className="plus">+</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  function AddCart(e) {
-    return console.log(('Add Item ' + e + ' Success'));
-  }
-  
-  CheckDiscount.propTypes = {
-    discount: PropTypes.number
-  };
-
-  function Header() {
-    return(
-      <div className="header">
-        <div className="troley"><br />
-            <div className="count">1</div>
-        </div>
-      </div>
-    )
   }
 
 export default App;
